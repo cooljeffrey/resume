4e7441a095eb7374437cb09863637f81f7751043
Jeffrey Yu <jeffrey.yu@temando.com>
2017-05-09 16:07:41 +1000

    introduced react-bootstrap and fixed the nav button on phone screen

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,44 +1,42 @@
-import React, { Component }  from 'react';
+import React, { Component } from "react";
+
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
 
 const navLinksStyle = {
-  minWidth: '87px'
+  minWidth: "87px"
 };
 
 class NavigationBar extends Component {
-  render(){
+  render() {
     return (
-      <nav className="navbar navbar-default navbar-fixed-top navbar-custom">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#main-navbar">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <a className="navbar-brand" href="https://cooljeffrey.github.io">Jeffrey Yu</a>
+      <Navbar fixedTop={true} fluid={true} className="navbar-custom">
+        <Navbar.Header>
+          <Navbar.Toggle />
+          <Navbar.Brand>
+            <a href="https://cooljeffrey.github.io">Jeffrey Yu</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem href="https://cooljeffrey.github.io">Home</NavItem>
+            <NavDropdown title="About Me" id="basic-nav-dropdown">
+              <MenuItem href="/aboutme">Profile</MenuItem>
+              <MenuItem href="/resume">Resume</MenuItem>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+        <div className="avatar-container">
+          <div className="avatar-img-border">
+            <a href="https://cooljeffrey.github.io ">
+              <img
+                className="avatar-img"
+                src="http://cooljeffrey.com/img/avatar-icon.jpg"
+                alt="avatar"
+              />
+            </a>
           </div>
-          <div className="collapse navbar-collapse" id="main-navbar">
-            <ul className="nav navbar-nav navbar-right">
-                <li><a href="https://cooljeffrey.github.io">Home</a></li>
-                <li className="navlinks-container" style={navLinksStyle}>
-                  <a className="navlinks-parent" href="#">About Me</a>
-                  <div className="navlinks-children">
-                    <a href="/aboutme">Profile</a>
-                    <a href="https://cooljeffrey.github.io/resume">Resume</a>
-                  </div>
-                </li>
-            </ul>
-          </div>
-        	<div className="avatar-container">
-        	  <div className="avatar-img-border">
-        	    <a href="https://cooljeffrey.github.io ">
-        	      <img className="avatar-img" src="http://cooljeffrey.com/img/avatar-icon.jpg" alt="avatar"/>
-        		  </a>
-        	  </div>
-        	</div>
         </div>
-      </nav>
+      </Navbar>
     );
   }
 }
