909d6d27f081ebb11a0acee71eba38c91cec6c12
Jeffrey Yu <jeffrey.yu@temando.com>
2019-07-15 10:06:23 +1000

    security fixes

---
+++ precache-manifest.23168f5b63f082ab5e868abcebfbbc9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9469bc2c6aec3dcc4eda2945a038a82c",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "606669c3c1c5c0f4417c",
+    "url": "/resume/static/css/main.ad2baa31.chunk.css"
+  },
+  {
+    "revision": "c3172e644200c0e088dc",
+    "url": "/resume/static/js/2.2a03e72f.chunk.js"
+  },
+  {
+    "revision": "606669c3c1c5c0f4417c",
+    "url": "/resume/static/js/main.d5660afb.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
